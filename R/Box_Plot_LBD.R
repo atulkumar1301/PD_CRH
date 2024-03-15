48a559d91abdf03a0439ede5fe02837d38b3b30f
ATUL KUMAR <atul.0298@gmail.com>
2024-03-15 11:58:17 +0100

    Update Box_Plot_LBD.R

--- R/Box_Plot_LBD.R
+++ R/Box_Plot_LBD.R
@@ -20,15 +20,15 @@ boxplot(CRH__Oncology_o2csf__NPX ~ Group, data = df_1, lwd = 2, ylab = 'CSF CRH'
 stripchart(CRH__Oncology_o2csf__NPX ~ Group, vertical = TRUE, data = df_1, 
            method = "jitter", add = TRUE, pch = 20, col = c('#E69F00', '#CC79A7'), cex = 2, jitter = 0.3)
 
-df_2 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Nicholas_Ashton/Data_SAA-AD_FTD_VaD_vs_SAA+_LBD.txt")
+df_2 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Nicholas_Ashton/Data_SAA-Ctrl_vs_SAA+Ctrl.txt")
 par(cex.axis=1.5)
-boxplot(CRH__Oncology_o2csf__NPX ~ Group, data = df_2, lwd = 2, ylab = 'CSF CRH', notch = TRUE, cex.lab = 1.5, main = "C) SAA- AD/FTD/VaD vs SAA+ LBD", cex.main = 1.5)
+boxplot(CRH__Oncology_o2csf__NPX ~ Group, data = df_2, lwd = 2, ylab = 'CSF CRH', notch = TRUE, cex.lab = 1.5, main = "C) SAA- CTR vs SAA+ CTR", cex.main = 1.5)
 stripchart(CRH__Oncology_o2csf__NPX ~ Group, vertical = TRUE, data = df_2, 
            method = "jitter", add = TRUE, pch = 20, col = c('#E69F00', '#CC79A7'), cex = 2, jitter = 0.3)
 
 
-df_3 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Nicholas_Ashton/Data_SAA-Ctrl_vs_SAA+Ctrl.txt")
+df_3 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Nicholas_Ashton/Data_SAA-AD_FTD_VaD_vs_SAA+_LBD.txt")
 par(cex.axis=1.5)
-boxplot(CRH__Oncology_o2csf__NPX ~ Group, data = df_3, lwd = 2, ylab = 'CSF CRH', notch = TRUE, cex.lab = 1.5, main = "D) SAA- CTR vs SAA+ CTR", cex.main = 1.5, col.main = "#0072B2")
+boxplot(CRH__Oncology_o2csf__NPX ~ Group, data = df_3, lwd = 2, ylab = 'CSF CRH', notch = TRUE, cex.lab = 1.5, main = "D) SAA- AD/FTD/VaD vs SAA+ LBD", cex.main = 1.5, col.main = "#0072B2")
 stripchart(CRH__Oncology_o2csf__NPX ~ Group, vertical = TRUE, data = df_3, 
            method = "jitter", add = TRUE, pch = 20, col = c('#E69F00', '#0072B2'), cex = 2, jitter = 0.3)
